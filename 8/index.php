4d972c0895ea399aa17b47ab719a18bf6092f55d
KERWIN2324 <118126514+KERWIN2324@users.noreply.github.com>
2024-10-04 09:31:53 -0400

    Add files via upload

---
+++ 8/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <title>Hola Mundo</title>
+</head>
+<body style="display: flex; justify-content: center;align-items: center;height: 100vh;margin: 0;font-family: Arial, sans-serif;background-color: #f0f0f0;">
+    <h1 style="font-size: 50px; color: #333;">
+        
+        
+    <?php echo "Hola mundo"; ?>
+    
+    
+
+    
+    </h1>
+</body>
+</html>
